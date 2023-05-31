7c87c26f9147fc2554f8699be3af7a76b5cf4c34
Ed Yu <edyu@users.noreply.github.com>
2023-05-31 15:33:16 -0700

    Add song list

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,7 +7,17 @@ import { useUser } from "@/hooks/useUser";
 import useAuthModal from "@/hooks/useAuthModal";
 import useUploadModal from "@/hooks/useUploadModal";
 
-const Library = () => {
+import MediaItem from "./MediaItem";
+
+import { Song } from "@/types";
+
+interface LibraryProps {
+    songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({
+    songs
+}) => {
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
     const { user } = useUser();
@@ -34,7 +44,9 @@ const Library = () => {
                 <AiOutlinePlus onClick={onClick} size={20} className="text-neutral-400 cursor-pointer hover:text-white transition" />
             </div>
             <div className="flex flex-col gap-y-2 mt-4 px-3">
-                List of Songs!
+                {songs.map((item) => (
+                    <MediaItem onClick={() => {}} key={item.id} data={item} />
+                ))}
             </div>
         </div>
     );
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            'nehubxukbfmrtznyqtzz.supabase.co'
+        ]
+    }
+}
 
 module.exports = nextConfig
